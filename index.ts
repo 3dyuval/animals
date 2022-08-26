c269b205f4b4cf1cb6373c11f7d924b88428e32f
3dyuval <3dyuval@gmail.com>
2022-08-26 14:42:49 +0300

    deno.readFile

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
-import {parse} from './parse.ts'
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { parse as parseCsv } from 'https://deno.land/std@0.82.0/encoding/csv.ts';
 
 serve( async (_req) => {
-    const animals = await parse()
+    const file = await Deno.readFile(`${Deno.cwd()}/animal_taxonomy.csv`)
+    const animals = await parseCsv(file, {
+          skipFirstRow: true,
+    })
   return new Response(JSON.stringify(animals), {
     headers: { "content-type": "text/plain" },
   });
--- parse.ts
+++
@@ -1,9 +0,0 @@
-import { parse as parseCsv } from 'https://deno.land/std@0.82.0/encoding/csv.ts';
-
-export const parse = async () => {
-    const file = await Deno.readFile('./animal_taxonomy.csv')
-    const animals = await parseCsv(file, {
-          skipFirstRow: true,
-    })
-    return animals
-}
