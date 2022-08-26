7b57f4fff280c02b2089822bea564dbb27a0ad82
3dyuval <3dyuval@gmail.com>
2022-08-26 14:54:32 +0300

    s

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { parse as parseCsv } from 'https://deno.land/std@0.82.0/encoding/csv.ts'
 serve( async (_req) => {
     const file = await Deno.readTextFile(`${Deno.cwd()}/animal_taxonomy.csv`)
     const animals = await parseCsv(file, {
-          yskipFirstRow: true,
+          skipFirstRow: true,
     })
   return new Response(JSON.stringify(animals), {
     headers: { "content-type": "text/plain" },
