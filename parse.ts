fa89507baa1443af285ceb13fc743124e8b5c03f
3dyuval <3dyuval@gmail.com>
2022-08-26 14:39:40 +0300

    deno.readFile

--- parse.ts
+++ parse.ts
@@ -1,7 +1,7 @@
 import { parse as parseCsv } from 'https://deno.land/std@0.82.0/encoding/csv.ts';
 
 export const parse = async () => {
-    const file =   Deno.readTextFileSync('animal_taxonomy.csv')
+    const file = await Deno.readFile('./animal_taxonomy.csv')
     const animals = await parseCsv(file, {
           skipFirstRow: true,
     })
